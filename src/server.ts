b3a4c4cacd304ba5dc3042245eb5a60a519dbb38
claudiomatos <cloud.devmo@gmail.com>
2024-05-12 12:52:06 -0300

    Debugando api

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ const app = express();
 const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
 app.use(express.json());
+app.use(express.urlencoded());
 
 app.use("/ping", exampleRoutes);
 app.use("/auth", authRoutes);
